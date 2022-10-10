7f5a980bd68ee9a6e2d4fd284647208edb116a5a
Benya Krik <isoba4kin@yandex.ru>
2022-10-11 00:01:55 +0300

    added base components and styles

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header-components />
+  <index-components />
+  <footer-components />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderComponent from './components/HeaderComponent.vue'
+import IndexComponent from './components/page/IndexComponent.vue'
+import FooterComponent from './components/FooterComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    'header-components': HeaderComponent,
+    'index-components': IndexComponent,
+    'footer-components': FooterComponent
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <header class="header">
+        <div class="header__container container">
+            <div class="header__logo">
+                <logo-component />
+            </div>
+            <div class="header__menu"></div>
+            <div class="header__social"></div>
+        </div>
+    </header>
+</template>
+
+<script>
+import LogoComponent from './ui/LogoComponent.vue'
+export default {
+  components: {
+    LogoComponent
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.header {
+    background-color: $mainColor;
+}
+</style>
